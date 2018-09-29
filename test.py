3d157fea48b3b8816902af09c0563d470852c8d2
Ryan <849908250@qq.com>
2018-09-29 22:02:23 +0800

    123

--- test.py
+++ test.py
@@ -1,18 +1,27 @@
 import pandas as pd
+import numpy as np
 
-# number= int( input("请输入列数："))
-#
-# df=pd.read_csv(path,usecols=[0,1,number])
-# print(df)
-#
-# print('----------------')
-def opendata(path,number):
-	df=pd.read_csv(path,usecols=[0,1,number])
 
-	print(df)
+def opendata(path, number):
+    df = pd.read_csv(path, usecols=[0, 1, number])
+    list_test=df.values.tolist()
 
-if __name__=='__main__':
+    print(list_test[16])
+    print(list_test[17])
+    print(list_test[38])
+    print(list_test[39])
+    print(list_test[40])
+    print(list_test[41])
+    print(list_test[42])
+    print(list_test[43])
+    print(list_test[45])
+    print(list_test[46])
+    print(list_test[47])
+    print(list_test[50])
+
+
+if __name__ == '__main__':
     path = r'D:/completed.csv'
-    number=int(input('请输入列数：'))
-    opendata(path,number)
-    i=input('看完了吗？')
+    number = int(input('请输入列数：'))
+    opendata(path, number)
+    i = input('看完了吗？')
